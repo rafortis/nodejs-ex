ac865bf1d5d5ec7a8fda97890afcb5fe3ea236de
Rafael <rafortis.dev@gmail.com>
2018-05-01 22:24:48 -0300

    test fix

--- tests/app_test.js
+++ tests/app_test.js
@@ -9,9 +9,9 @@ reqServer = process.env.HTTP_TEST_SERVER || server
 
 describe('Basic routes tests', function() {
 
-    it('GET to / should return 200', function(done){
+    it('GET to /api should return 200', function(done){
         chai.request(reqServer)
-        .get('/')
+        .get('/api')
         .end(function(err, res) {
             res.should.have.status(200);
             done();
