76d815fb504e5c4a3aaf367dfe0524cdbbf6b235
Rafael <rafortis.dev@gmail.com>
2018-05-01 22:35:37 -0300

    cors

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 var express = require('express'),
   app = express(),
   morgan = require('morgan');
+var cors = require('cors')
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose'); // ORM
 
@@ -15,6 +16,8 @@ Object.assign = require('object-assign');
 app.use(morgan('combined'));
 //app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.use(cors());
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
   ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
